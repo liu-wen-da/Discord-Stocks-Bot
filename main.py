8b06d1b66372b0dc4ce9d2b22e9e5de46e40e42a
wendaliu <321wenda@gmail.com>
2022-03-04 12:46:05 -0800

    added a function that will read the Json file

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import discord 
+import json
 
 
 # open a connection with discord 
-
 client = discord.Client()
+
 # print a statment in the chat that the bot is logged in as this person 
 @client.event 
 async def on_ready():
     print('I have logged in as {0.user}'.format(client))
-
 
 
 # if a user in the channel types hellow the bot will pick up detected the hello and reply its own hello
@@ -22,6 +22,15 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
 
-
 # client.run('what ever our token will be')
 # this will run the codes when we have everything setup 
+
+# this will ope na json file 
+with open ('.//Yahoo_Json','r') as f:
+    outfile = open('.//Test_file.txt', 'w') # use for test
+    line = f.readline()
+    count_line = 0
+    while line:
+        data = json.loads(line)
+        outfile.write() # test if our parseing are what we need
+
