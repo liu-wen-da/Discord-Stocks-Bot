55104b7bd7a082aed2e02bf5e641ea43ff954b67
wendaliu <321wenda@gmail.com>
2022-03-03 17:06:26 -0800

    New comments

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import discord
 
 
 # open a connection with discord 
+
 client = discord.Client()
 # print a statment in the chat that the bot is logged in as this person 
 @client.event 
 async def on_ready():
     print('I have logged in as {0.user}'.format(client))
-# if a user in the channel types hellow the bot will pick up detected the hello and repy
-# its own hello 
+
+
+
+# if a user in the channel types hellow the bot will pick up detected the hello and reply its own hello
+# This is how the bot will display our message 
+# So anything we want the bot to display in the channel chat is gonna be here  
 @Client.event 
 async def on_message(message):
     if message.author == client.user:
@@ -16,6 +21,7 @@ async def on_message(message):
     if message.content.startwith('$hello'):
         await message.channel.send('Hello!')
 
+
+
 # client.run('what ever our token will be')
 # this will run the codes when we have everything setup 
-# test
